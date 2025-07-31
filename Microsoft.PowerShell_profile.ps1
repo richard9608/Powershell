8b238ba533bbcdac76d4383c9cb916e97a24330b
Lavar Richardson <lrichardson96@gmail.com>
2025-07-31 16:50:41 -0400

    Added set-PSReadLineOption -PredictionViewStyle ListView

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,32 @@
+set-PSReadLineOption -PredictionViewStyle ListView
 #--------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------
+#Record PowerShell session logs
+
+function Record-SessionLogsLR {
+    param([string]$ScriptName)
+
+    $logFile = "C:\Users\LRichardson2_adm\Documents\PS Session Logs\$ScriptName-$(Get-Date -Format 'yyyy-MM-dd_HHmmss').txt"
+    try {
+        Start-Transcript -Path $logFile -ErrorAction Stop
+        return $true
+    } catch {
+        Write-Host "Transcript already running. Skipping." -ForegroundColor Yellow
+        return $false
+    }
+}
+
+function Stop-SafeTranscript {
+    param([bool]$ShouldStop)
+    if ($ShouldStop) {
+        Stop-Transcript
+    }
+}
+
 #--------------------------------------------------------------------------------------------
 
 # New-ADGroup
